34b37ad17c7550af0ccda5e8e26373691e9b21e1
Antonio Rodriguez <102554647+antoniorodr@users.noreply.github.com>
2026-04-13 14:05:11 +0200

    Add boilerplate code

--- lua/lexy.lua
+++ lua/lexy.lua
@@ -1,12 +1,32 @@
 local M = {}
+local lexy_local_docs = os.getenv("HOME") .. "/.config/lexy/files"
+
+M.search = function(query)
+	-- This function will search for the query in the lexy_local_docs directory and return the file
+	if not query or query == "" then
+		vim.notify("Please provide a search query.")
+		return
+	end
+
+	for file in vim.fs.dir(lexy_local_docs) do
+		if file:match(query) then
+			return vim.fs.joinpath(lexy_local_docs, file)
+		end
+	end
+end
 
 M.list = function()
-	vim.system("lexy list")
+	--This function will do the same as `Lexy list`command, but it will be opened with the neovim picker (snacks/telescope)
 end
 
 M.setup = function(opts)
 	opts = opts or {}
-	vim.api.nvim_create_user_command("LexyList", M.list, {})
+	vim.api.nvim_create_user_command("LexySearch", function(opts)
+		M.search(opts.args)
+	end, {
+		desc = "Search for a file in lexy",
+		nargs = 1,
+	})
 
 	local keymap = opts.keymap or "<leader>sl"
 
