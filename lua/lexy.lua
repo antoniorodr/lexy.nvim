cc53495f623505cab6e2a8293202fbbaeb7bda5d
Antonio Rodriguez <102554647+antoniorodr@users.noreply.github.com>
2026-04-13 15:20:35 +0200

    Move common code from lexy to common lua file

--- lua/lexy.lua
+++ lua/lexy.lua
@@ -1,5 +1,4 @@
 local M = {}
-local lexy_local_docs = os.getenv("HOME") .. "/.config/lexy/files"
 
 M.search = function(query)
 	-- This function will search for the query in the lexy_local_docs directory and return the file
@@ -8,19 +7,17 @@ M.search = function(query)
 		return
 	end
 
-	for file in vim.fs.dir(lexy_local_docs) do
-		if file:match(query) then
-			return vim.fs.joinpath(lexy_local_docs, file)
-		end
-	end
+	--TODO: Search for the query and present items in a picker (snacks/telescope)
 end
 
 M.list = function()
 	--This function will do the same as `Lexy list`command, but it will be opened with the neovim picker (snacks/telescope)
+	--TODO: List all the items in the lexy_local_docs directory and present them in a picker (snacks/telescope)
 end
 
 M.setup = function(opts)
 	opts = opts or {}
+	-- vim.api.nvim_create_user_command("LexyList", M.list, {})
 	vim.api.nvim_create_user_command("LexySearch", function(opts)
 		M.search(opts.args)
 	end, {
---
+++ lua/lexy/common.lua
@@ -0,0 +1,22 @@
+local function data_folder()
+	return os.getenv("HOME") .. "/.config/lexy/files/"
+end
+
+local function filename_to_display(filename)
+	return filename:match("^[^.]+") or filename
+end
+
+local function get_lexy_docs()
+	local docs = {}
+	for file in vim.fs.dir(data_folder()) do
+		local filename = filename_to_display(file)
+		docs[filename] = vim.fs.joinpath(data_folder(), file)
+	end
+	return docs
+end
+
+return {
+	data_folder = data_folder,
+	filename_to_display = filename_to_display,
+	get_lexy_docs = get_lexy_docs,
+}
